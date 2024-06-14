487c1a2858362b7114ca4a941e43e0087eb755bf
binarylover <whitedevil.hamd@gmail.com>
2024-06-14 23:40:00 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -6,11 +6,6 @@ const port = dotenv.config().parsed.PORT || 4000
 app.get('/', (req, res) => {
   res.send('Hello Mern!')
 })
-app.get('/test', (req, res) => {
-    res.send('Test')
-    }
-) 
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
